6d7ed48a54d6deca66506102ed8cbe36882534d0
hoyeonjigi.lee@samsung.com <hoyeonjigi.lee@samsung.com>
2015-04-04 13:58:55 +0900

    appservice update

--- minion-agent/src/com/sds/minion/agent/Application.java
+++ minion-agent/src/com/sds/minion/agent/Application.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @SpringBootApplication
 public class Application {
@@ -23,6 +25,14 @@ public class Application {
         }
         
         System.out.println("Agent up up!");
+    }
+
+    @Bean
+    public CharacterEncodingFilter characterEncodingFilter() {
+        final CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return characterEncodingFilter;
     }
 
 }
