464644263a0b8e92a8ebea3e663515cf30e4601b
alm <alm-support@samsung.com>
2015-08-28 18:51:02 +0900

    test

---
+++ minion-agent/src/com/sds/minion/agent/web/FileRecieverController.java
@@ -0,0 +1,65 @@
+package com.sds.minion.agent.web;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@SuppressWarnings("serial")
+@WebServlet("/recieve")
+@MultipartConfig(fileSizeThreshold = 1024 * 1024 * 2)
+public class FileRecieverController {
+  
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    //String savePath = request.getHeader("dest"); //목적지입니다.
+    String savePath = "D:/tmp/";
+    savePath = savePath.replace("\\\\", "/");
+    File fileSaveDir = new File(savePath);
+    if (!fileSaveDir.exists()) {
+      if (fileSaveDir.mkdirs()) {
+        // log.info("Make success");
+      } else {
+        System.out.println("Make Failed:[" + fileSaveDir + "]");
+      }
+    }
+
+    for (Part part : request.getParts()) {
+      String fileName = extractFileName(part);
+      if (fileName != null && !fileName.equals("")) {
+        part.write(savePath + File.separator + fileName);
+        System.out.println("UploadedFile:" + fileName + "(" + FileUtils.byteCountToDisplaySize(part.getSize()) + ")");
+      }
+    }
+
+    request.setAttribute("message", "Upload has been done successfully!");
+    response.setContentType("text/javascript");
+    PrintWriter out = response.getWriter();
+    out.print("OK");
+    out.flush();
+  }
+
+  /**
+   * Extracts file name from HTTP header content-disposition
+   */
+  private String extractFileName(Part part) {
+    String contentDisp = part.getHeader("content-disposition");
+    String[] items = contentDisp.split(";");
+    for (String s : items) {
+      if (s.trim().startsWith("filename")) {
+        return s.substring(s.indexOf("=") + 2, s.length() - 1);
+      }
+    }
+    return "";
+  }
+}
